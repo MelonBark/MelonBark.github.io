fa0d2832e9e38a50eaafffa5bbb0ff00bd515ade
Mark Noble <3819700+marknoble@users.noreply.github.com>
2022-12-21 02:07:34 -0500

    Starting fresh on a cleaner page design with the help fo GPT.

---
+++ script/gpt-script.js
@@ -0,0 +1,12 @@
+﻿const header = document.querySelector('header');
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+    let currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        header.style.height = '80px';
+    } else {
+        header.style.height = '60px';
+    }
+    prevScrollpos = currentScrollPos;
+}
